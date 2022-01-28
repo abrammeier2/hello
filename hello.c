1d3b8e576d9e9b6663d4b4e795679ec9a8723cda
abrammei <abrammeier2@huskers.unl.edu>
2022-01-28 15:56:24 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Aidan Brammeier
+ * Date: 2021/01/28
+ *
+ * A simple hello program in C
+ *
+ */
+
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Aidan Brammeier\n");
+
+     return 0;
+ }
